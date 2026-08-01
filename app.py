ccb5cbc41deeb4ff1f04f4de5acca139bc80a9a3
Nitheshk42 <nitheshk42@gmail.com>
2026-08-01 09:29:22 -0400

    Fix chromadb sqlite3 version issue on Streamlit Cloud

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+__import__("pysqlite3")
+import sys
+sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+
 import streamlit as st
 from src.visual_pipeline import display_visual_pipeline
 from src.chat import display_chat_interface
